7efce100690b985072f7f7e36923976084aa4553
Michael <143526295+mgj0226@users.noreply.github.com>
2025-07-04 10:48:18 +0100

    rebuild with next.js

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# mgj0226.github.io
-Hi, I am Michael. I am a web developer.
-This is my portfolio. Here you can find projects I have done and details about myself.
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="font-[family-name:var(--font-geist-mono)] text-4xl font-bold">
+          Welcome to My Portfolio
+        </h1>
+        <p className="font-[family-name:var(--font-geist-mono)] text-lg">
+          I'm Michael Chan, a developer with a passion for building
+          web applications. Here, you'll find my projects, ideas, and
+          insights into my work.
+        </p>
+      </main>
+    </div>
+  );
+}
