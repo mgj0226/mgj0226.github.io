79d5c46327430321b242bc3245b612b1df5efb35
Michael <143526295+mgj0226@users.noreply.github.com>
2026-02-24 21:06:48 +0000

    added css and gif
    css for header, footer, app
    animation gif to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import Pilibon from '../assets/Pilibon2.gif';
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 Michael's Portfolio</p>
+      <p>&copy; 2026 Michael's Portfolio
+        <img src={Pilibon} alt="Pilibon" className="footer-gif" />
+      </p>
     </footer>
   );
 }
