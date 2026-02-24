476ea479ad1ad2fae165d4f6076f1a11e428b59f
Michael <143526295+mgj0226@users.noreply.github.com>
2026-02-24 10:00:52 +0000

    Basic HomePage and added ProjectsPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/MainComponents/About';
+import Projects from './components/MainComponents/Projects';
 
 function App() {
   return (
