e7763d68169f0f36ab0b3cc93d96948414519ff6
Michael <143526295+mgj0226@users.noreply.github.com>
2026-04-02 11:32:05 +0100

    header and footer height adjust

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ import Pilibon from '../assets/Pilibon2.gif';
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 Michael's Portfolio
+      <p>
         <img src={Pilibon} alt="Pilibon" className="footer-gif" />
+        &copy; 2026 Michael's Portfolio
       </p>
     </footer>
   );
