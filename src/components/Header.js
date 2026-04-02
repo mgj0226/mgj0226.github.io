cdbe4814952418d51ef960e556b11e6f17a10e0c
Michael <143526295+mgj0226@users.noreply.github.com>
2026-04-02 14:59:00 +0100

    header and icon update
    added nav, updated css grid, fixed the icon ( white background)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import icon from '../assets/wolfLogo4.svg';
 export default function Header() {
   return (
     <header className="header">
-      <h1>Michael Chan</h1>
+      <img src={icon} alt="Icon" className='icon' />
+      <h1 className='name'>Michael Chan</h1>
+      <nav className='nav'>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
     </header>
   );
 }
