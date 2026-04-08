d4d8eaee3991678cd98603442ef0b0abe0386906
Michael <143526295+mgj0226@users.noreply.github.com>
2026-04-08 12:28:17 +0100

    Project page update
    setting basic layout. added LittleLemon

--- src/components/MainComponents/Projects.js
+++ src/components/MainComponents/Projects.js
@@ -1,8 +1,21 @@
+import LittleLemonIcon from "../../assets/LittleLemon.png";
+
 export default function Projects() {
   return (
-    <div id="projects" className="projects">
-      <h2>Projects</h2>
-      <p>Here are some of my projects:</p>
+    <div className="projects">
+      <section id="projects" className="projects-title">
+        <h2>Projects</h2>
+        <p>Here are some of my projects:</p>
+      </section>
+      <section className="project-list">
+        <div className="project-item">
+          <img 
+          src={LittleLemonIcon} 
+          alt="Little Lemon logo" 
+          className="project-image" />
+          <h2>Little Lemon</h2>
+        </div>
+      </section>
     </div>
   );
 }
